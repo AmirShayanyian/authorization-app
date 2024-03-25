029f8483f92bada4fa1c0402b5e01f7c81d723d3
amirshayanyian <wahtamir12@gmail.com>
2024-03-25 14:42:25 +0330

    added a verify pass function

--- utils/auth.util.js
+++ utils/auth.util.js
@@ -1,1 +1,13 @@
-const bcrypt = require('bcr');
+const bcrypt = require('bcrypt');
+
+function hashPassword(password) {
+  const salt = bcrypt.genSaltSync(12);
+  const hash = bcrypt.hashSync(password, salt);
+  return hash;
+}
+
+function verifyPassword(password, hashedPassword) {
+  const verified = bcrypt.compareSync(password, hashedPassword);
+  if (verified) return true;
+  throw new Error('Password is incorrect');
+}
